7cbb7217636aabff892c29ddf62e649523ec9159
hawejdene <hawejdene@gmail.com>
2020-01-16 00:35:44 +0100

    fix side bar

--- src/app/_shared/shared-module/shared-module.module.ts
+++ src/app/_shared/shared-module/shared-module.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DefaultImageBookPipe} from '../../pipes/default-image-book.pipe';
-
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 @NgModule({
   declarations: [DefaultImageBookPipe,
   ],
   imports: [
+    NzMenuModule,
     CommonModule
   ],
   exports: [
