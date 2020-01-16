535b4922ffc4b602c8a3279dd36c30c79a06afde
rached1997 <hawejdene@gmail.com>
2020-01-16 01:24:09 +0100

    merge

--- src/app/globals/global-variables.ts
+++ src/app/globals/global-variables.ts
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:8080';
+export const API_URL = 'http://localhost:8081';
 export const SIGN_IN = '/api/auth/signin';
 export const SIGN_UP = '/api/auth/signup';
 export const DASHBORD = '/dashboard';
